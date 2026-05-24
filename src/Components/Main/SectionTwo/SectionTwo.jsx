b5eb8aa8e8ace1cb4c0d9f19842e7c81830a64d0
Mahliyo <gasandigital13@gmail.com>
2026-05-24 19:07:41 +0500

    sectionTwo wrapper css finish

--- src/Components/Main/SectionTwo/SectionTwo.jsx
+++ src/Components/Main/SectionTwo/SectionTwo.jsx
@@ -1,10 +1,18 @@
+import { useTranslation } from "react-i18next";
 import "./SectionTwo.css";
 import React from 'react'
 
 function SectionTwo() {
-    const
+    const [t, i18n] = useTranslation();
   return (
-    <div>SectionTwo</div>
+    <div className="sectionTwo">
+        <div className="container">
+            <div className="sectionTwo-wrapper">
+                <h2 className="two-title"></h2>
+                <p className="two-text"></p>
+            </div>
+        </div>
+    </div>
   )
 }
 
