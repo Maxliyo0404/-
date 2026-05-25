c913c7c361c36b65befcd2e8ab86adbc946d4d25
Mahliyo <gasandigital13@gmail.com>
2026-05-25 22:19:19 +0500

    sectionThree css

--- src/Components/Main/SectionThree/SectionThree.jsx
+++ src/Components/Main/SectionThree/SectionThree.jsx
@@ -2,11 +2,12 @@ import "./SectionThree.css";
 import React from 'react'
 
 function SectionThree() {
+    const [t, i18n] =
   return (
     <div className="sectionThree">
         <div className="container">
             <div className="sectionThree-wrapper">
-                
+
             </div>
         </div>
     </div>
