d2b0e01dfe01c5170689ba61f649f58877a79f90
Mahliyo <gasandigital13@gmail.com>
2026-05-25 22:49:28 +0500

    sectionFive-css

--- src/Components/Main/SectionThree/SectionThree.jsx
+++ src/Components/Main/SectionThree/SectionThree.jsx
@@ -21,6 +21,8 @@ function SectionThree() {
                     <div className="list-icons">
                         <div className="icon-list"><CiUser /></div>
                         <h3 className="list-title">5.8 k</h3>
+                        <div className="sectionThree"></div>
+
                     </div>
                 </li>
             </ul>
