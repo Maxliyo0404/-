965f3cca5fddc7a26128e241d6451204fd171ae0
Mahliyo <gasandigital13@gmail.com>
2026-05-26 23:10:20 +0500

    sectionfour

--- src/Components/Main/SectionThree/SectionThree.jsx
+++ src/Components/Main/SectionThree/SectionThree.jsx
@@ -31,6 +31,82 @@ function SectionThree() {
                     <h4 className="threeList-title">{t("sectionThree.threeList-title1")}<span className="threeList-span">$150</span></h4>
                     <p className="threeList-text">{t("sectionThree.threeList-text")}</p>
                 </li>
+                <li className="three-item">
+                    <img className="three-img" src={} alt="rasm" />
+                    <div className="list-icons">
+                        <div className="icon-list"><CiUser /></div>
+                        <h3 className="list-title">5.8 k</h3>
+                        <div className="sectionThree"><TfiEye />Jgit add </div>
+                        <h3 className="list-title">69k</h3>
+                         <div className="icon-list"><CiStar /></div>
+                          <h3 className="list-title">4.3k</h3>
+                          <img className="list-img" src={} alt="rasm" />
+                        
+                    </div>
+                    <h4 className="threeList-title">{t("sectionThree.threeList-title1")}<span className="threeList-span">$150</span></h4>
+                    <p className="threeList-text">{t("sectionThree.threeList-text")}</p>
+                </li>
+                <li className="three-item">
+                    <img className="three-img" src={} alt="rasm" />
+                    <div className="list-icons">
+                        <div className="icon-list"><CiUser /></div>
+                        <h3 className="list-title">5.8 k</h3>
+                        <div className="sectionThree"><TfiEye />Jgit add </div>
+                        <h3 className="list-title">69k</h3>
+                         <div className="icon-list"><CiStar /></div>
+                          <h3 className="list-title">4.3k</h3>
+                          <img className="list-img" src={} alt="rasm" />
+                        
+                    </div>
+                    <h4 className="threeList-title">{t("sectionThree.threeList-title1")}<span className="threeList-span">$150</span></h4>
+                    <p className="threeList-text">{t("sectionThree.threeList-text")}</p>
+                </li>
+                <li className="three-item">
+                    <img className="three-img" src={} alt="rasm" />
+                    <div className="list-icons">
+                        <div className="icon-list"><CiUser /></div>
+                        <h3 className="list-title">5.8 k</h3>
+                        <div className="sectionThree"><TfiEye />Jgit add </div>
+                        <h3 className="list-title">69k</h3>
+                         <div className="icon-list"><CiStar /></div>
+                          <h3 className="list-title">4.3k</h3>
+                          <img className="list-img" src={} alt="rasm" />
+                        
+                    </div>
+                    <h4 className="threeList-title">{t("sectionThree.threeList-title1")}<span className="threeList-span">$150</span></h4>
+                    <p className="threeList-text">{t("sectionThree.threeList-text")}</p>
+                </li>
+                <li className="three-item">
+                    <img className="three-img" src={} alt="rasm" />
+                    <div className="list-icons">
+                        <div className="icon-list"><CiUser /></div>
+                        <h3 className="list-title">5.8 k</h3>
+                        <div className="sectionThree"><TfiEye />Jgit add </div>
+                        <h3 className="list-title">69k</h3>
+                         <div className="icon-list"><CiStar /></div>
+                          <h3 className="list-title">4.3k</h3>
+                          <img className="list-img" src={} alt="rasm" />
+                        
+                    </div>
+                    <h4 className="threeList-title">{t("sectionThree.threeList-title1")}<span className="threeList-span">$150</span></h4>
+                    <p className="threeList-text">{t("sectionThree.threeList-text")}</p>
+                </li>
+                <li className="three-item">
+                    <img className="three-img" src={} alt="rasm" />
+                    <div className="list-icons">
+                        <div className="icon-list"><CiUser /></div>
+                        <h3 className="list-title">5.8 k</h3>
+                        <div className="sectionThree"><TfiEye />Jgit add </div>
+                        <h3 className="list-title">69k</h3>
+                         <div className="icon-list"><CiStar /></div>
+                          <h3 className="list-title">4.3k</h3>
+                          <img className="list-img" src={} alt="rasm" />
+                        
+                    </div>
+                    <h4 className="threeList-title">{t("sectionThree.threeList-title1")}<span className="threeList-span">$150</span></h4>
+                    <p className="threeList-text">{t("sectionThree.threeList-text")}</p>
+                </li>
+                
             </ul>
             </div>
         </div>
