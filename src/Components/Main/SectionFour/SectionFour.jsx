7035949743c40667a892cd29df30de99a799fe8b
Mahliyo <gasandigital13@gmail.com>
2026-05-27 16:07:06 +0500

    sectionSix

--- src/Components/Main/SectionFour/SectionFour.jsx
+++ src/Components/Main/SectionFour/SectionFour.jsx
@@ -4,6 +4,8 @@ import React from 'react'
 import four_img1 from "./image/four_img1.svg";
 import four_img2 from "./image/four_img2.svg";
 import four_img3 from "./image/four_img3.svg";
+import { IoStarSharp } from "react-icons/io5";
+import { FaRegUser } from "react-icons/fa";
 
 function SectionFour() {
     const [t, i18n] = useTranslation();
@@ -18,58 +20,70 @@ function SectionFour() {
                 <ul className="four-list">
                     <li className="four-item">
                         <img src={four_img1} alt="rasm" />
+                        <div className="four-items">
                         <h3 className="fourList-title">{t("sectionFour.fourList-title1")}</h3>
                         <p className="fourList-texte">{t("sectionFour.fourList-texte1")}</p>
                         <div className="four-icons">
                             <div className="four-icon">
                                 <div className="fourList-icons">
-                                 <div className="fourList-user">
+                                 <div className="fourList-users">
+                                    <div className="four-user"><FaRegUser /></div>
                                     <p className="fourlist-text">8.2k</p>
                                  </div>
-                                 <div className="fourList-star">
+                                 <div className="fourList-stars">
+                                    <div className="four-star"><IoStarSharp /></div>
                                     <p className="fourlist-text">4.5</p>
                                  </div>
                                 </div>
                             </div>
                             <p className="fourlist-text">{t("sectionFour.fourList-text")}</p>
+                        </div>
                         </div>
                         
                     </li>
                     <li className="four-item">
                         <img src={four_img2} alt="rasm" />
+                        <div className="four-items">
                         <h3 className="fourList-title">{t("sectionFour.fourList-title2")}</h3>
                         <p className="fourList-texte">{t("sectionFour.fourList-texte2")}</p>
                         <div className="four-icons">
                             <div className="four-icon">
                                 <div className="fourList-icons">
-                                 <div className="fourList-user">
+                                 <div className="fourList-users">
+                                    <div className="four-user"><FaRegUser /></div>
                                     <p className="fourlist-text">8.2k</p>
                                  </div>
-                                 <div className="fourList-star">
+                                 <div className="fourList-stars">
+                                     <div className="four-star"><IoStarSharp /></div>
                                     <p className="fourlist-text">4.5</p>
                                  </div>
                                 </div>
                             </div>
                             <p className="fourlist-text">{t("sectionFour.fourList-text")}</p>
                         </div>
+                        </div>
                         
                     </li>
                     <li className="four-item">
                         <img src={four_img3} alt="rasm" />
+                        <div className="four-items">
                         <h3 className="fourList-title">{t("sectionFour.fourList-title1")}</h3>
                         <p className="fourList-texte">{t("sectionFour.fourList-texte2")}</p>
                         <div className="four-icons">
                             <div className="four-icon">
                                 <div className="fourList-icons">
-                                 <div className="fourList-user">
+                                 <div className="fourList-users">
+                                    <div className="four-user"><FaRegUser /></div>
                                     <p className="fourlist-text">8.2k</p>
                                  </div>
-                                 <div className="fourList-star">
+                                 <div className="fourList-stars">
+                                     <div className="four-star"><IoStarSharp /></div>
                                     <p className="fourlist-text">4.5</p>
                                  </div>
                                 </div>
                             </div>
                             <p className="fourlist-text">{t("sectionFour.fourList-text")}</p>
+                        </div>
                         </div>
                         
                     </li>
