9eeaa0fafa459533a04a739613780795fc8dd829
Mahliyo <gasandigital13@gmail.com>
2026-05-27 12:49:08 +0500

    sectionFive wrapperresponsive

--- src/Components/Main/SectionFour/SectionFour.jsx
+++ src/Components/Main/SectionFour/SectionFour.jsx
@@ -5,7 +5,24 @@ import React from 'react'
 function SectionFour() {
     const [t, i18n] = useTranslation();
   return (
-    <div>SectionFour</div>
+    <div className="sectionFour">
+        <div className="container">
+            <div className="sectionFour-wrapper">
+                <div className="four-titles">
+                    <h2 className="four-title">{t("")}</h2>
+                    <p className="four-text"></p>
+                </div>
+                <ul className="four-list">
+                    <li className="four-item">
+                        <img src={} alt="rasm" />
+                        <h3 className="fourList-title"></h3>
+                        <p className="fourList-text"></p>
+                        
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
   )
 }
 
