e0dc36318b372f32b41cac4ee546019d73f6e70c
Mahliyo <gasandigital13@gmail.com>
2026-05-28 10:47:49 +0500

    sectionNine

--- src/Components/Main/SectionNine/SectionNine.jsx
+++ src/Components/Main/SectionNine/SectionNine.jsx
@@ -1,7 +1,9 @@
+import { useTranslation } from "react-i18next";
 import "./SectionNine.css";
 import React from 'react'
 
 function SectionNine() {
+    const [t, i18n] = useTranslation();
   return (
     <div>SectionNine</div>
   )
